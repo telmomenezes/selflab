abf68938292c7de41f14819685675046b03f002f
telmomenezes <telmo@telmomenezes.com>
2016-07-10 19:44:03 +0200

    event type

--- selflab/web/newevent.py
+++ selflab/web/newevent.py
@@ -18,8 +18,7 @@ def html():
 
 
 def add(db, name, quantity, value, details):
-    # print('%s %s %s %s' % (name, quantity, value, details))
     ts = int(time.time())
-    db.execute('INSERT INTO event (ts, name, quantity, value, details) VALUES (?, ?, ?, ?, ?)',
-               (ts, name, int(quantity), float(value), details))
+    db.execute('INSERT INTO event (ts, name, type, quantity, value, details) VALUES (?, ?, ?, ?, ?)',
+               (ts, name, 0, int(quantity), float(value), details))
     db.commit()
