afc92c2316f03e9cf81f96cc534eb7751e4b72ac
telmomenezes <telmo@telmomenezes.com>
2016-07-12 10:44:12 +0200

    fixing setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='selflab',
     version='0.1',
-    packages=find_packages(),
+    packages=['selflab'],
     install_requires=[
         'numpy',
         'scipy',
